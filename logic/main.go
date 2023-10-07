dec27ac4f696be208993436adc7e27dad85def92
salehrashid <saleh.rashid.b@gmail.com>
2023-10-07 17:29:03 +0700

    add first partials template

--- logic/main.go
+++ logic/main.go
@@ -25,8 +25,20 @@ func httpHandler() {
 	fmt.Println("server nya lagi jalan nih bang, http://localhost:7000")
 
 	http.HandleFunc("/car", car)
+	http.HandleFunc("/index", master)
 
 	log.Fatal(http.ListenAndServe(":7000", nil))
+}
+
+func master(writer http.ResponseWriter, _ *http.Request) {
+	tmplt := template.Must(template.ParseFiles(
+		"template/master.html",
+		"template/partials/_template1.html",
+		"template/partials/_template2.html",
+	))
+	if err := tmplt.Execute(writer, nil); err != nil {
+		log.Fatal(err)
+	}
 }
 
 func car(writer http.ResponseWriter, _ *http.Request) {
